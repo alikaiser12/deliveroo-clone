cc86493b6dc2122754b4697ad6586e5cced1da6e
Ali <rimu.alikaiser@gmail.com>
2024-07-11 22:02:10 +0600

    icon

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -77,6 +77,22 @@ const TabLayout = () => {
           }}
         
         />
+
+<Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
       </Tabs>
     </>
   )
