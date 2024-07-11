1260c95f9ff333fb715c8c994f64b12135a5e0ae
Ali <rimu.alikaiser@gmail.com>
2024-07-11 21:49:42 +0600

    Icons

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,12 +1,14 @@
 import { Tabs } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-
-
-
 import {Text,View} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Profile from './(tabs)/profile';
 import Homescreen from '../screens/Homescreen';
+import { Link } from 'expo-router';
+
+
+
 
 
 const App=()=>{
@@ -14,7 +16,9 @@ const App=()=>{
   return(
     
         <View className="flex-1 items-center justify-center bg-white">
-            <Text > <Homescreen/> </Text>
+            <Text className="text-3xl font-pblack"> <Homescreen/> </Text>
+            <Link href="./home" className='bg-blue-300'>Home</Link>
+            
             <StatusBar style='auto'/>
         </View>
     
