d8d71528db287b5955cb1e72911825c1e984e257
Ali <rimu.alikaiser@gmail.com>
2024-07-12 10:15:22 +0600

    logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {Text,View} from 'react-native';
+import {Text,View, Image, ScrollView} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Profile from './(tabs)/profile';
 
 import { Link } from 'expo-router';
 
 
+import {images} from '../constants';
 
 
 
@@ -15,14 +16,19 @@ const App=()=>{
     
   return(
     
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-plight"> Welcome to Learn Ling </Text>
-            <Link href="./home" className='bg-blue-300'>Home</Link>
-            
-            <StatusBar style='auto'/>
-        </View>
-    
-    
+        <SafeAreaView>
+          <ScrollView  contentContainerStyle={{height:'100%'}}>
+            <View className="w-full justify-center h-full px-20">
+              <Image
+                source={images.logo}
+                className="w-[200px] h-[150px]"
+                resizeMode="contain"
+              />
+              <Text className="py-4 px-7 font-plight text-2xl font-bold text-center ">Welcome to LearnLing</Text>
+            </View>
+          </ScrollView>
+
+        </SafeAreaView>  
 
   )
 }
